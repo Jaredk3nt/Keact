6681d674fc9dbf829de99583ae6c3f32b2d86eb6
Jared Jones <Jared.Jones@cdk.com>
2019-11-19 14:13:40 -0600

    Starting on newer tutorial

--- src/keact.js
+++ src/keact.js
@@ -1,50 +1,101 @@
-function render(element, parent) {
-    let dom;
-    console.log({element})
-    if (typeof element === 'string') {
-        dom = document.createTextNode(element);
-    } else {
-        const {
-            type,
-            props,
-            children
-        } = element;
+// function render(element, parent) {
+//   let dom;
+//   console.log({ element });
+//   if (typeof element === "string") {
+//     dom = document.createTextNode(element);
+//   } else {
+//     const { type, props, children } = element;
 
-        dom = document.createElement(type);
-        const propsObj = props || {};
-        const isListener = name => name.startsWith("on");
-        Object.keys(propsObj).filter(isListener).forEach(name => {
-            const eventType = name.toLowerCase().substring(2);
-            dom.addEventListener(eventType, propsObj[name]);
-        });
+//     dom = document.createElement(type);
+//     const propsObj = props || {};
+//     const isListener = name => name.startsWith("on");
+//     Object.keys(propsObj)
+//       .filter(isListener)
+//       .forEach(name => {
+//         const eventType = name.toLowerCase().substring(2);
+//         dom.addEventListener(eventType, propsObj[name]);
+//       });
 
-        const isAttribute = name => !isListener(name);
-        Object.keys(propsObj).filter(isAttribute).forEach(name => {
-            dom[name] = propsObj[name];
-        });
+//     const isAttribute = name => !isListener(name);
+//     Object.keys(propsObj)
+//       .filter(isAttribute)
+//       .forEach(name => {
+//         dom[name] = propsObj[name];
+//       });
 
-        const childElements = children || [];
-        childElements.forEach(childElement => render(childElement, dom));
+//     const childElements = children || [];
+//     childElements.forEach(childElement => render(childElement, dom));
+//   }
+//   console.log({ dom });
+//   parent.appendChild(dom);
+// }
+
+function createElement(type, props = {}, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child =>
+        typeof child === "object" ? child : createTextElement(child)
+      )
     }
-    console.log({dom})
-    parent.appendChild(dom);
+  };
 }
 
-function createElement(type, props = {}, ...args) {
-    const children = [...args].filter(arg => arg);
-    return {
-        type,
-        props,
-        children
-    };
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  };
+}
+
+function render(element, container) {
+  let dom;
+  // Determine type of node to create
+  switch (element.type) {
+    case "TEXT_ELEMENT":
+      dom = document.createTextNode("");
+      break;
+    default:
+      dom = document.createElement(element.type);
+      element.props.children.forEach(child => render(child, dom));
+  }
+  // Attach HTML properties to the node
+  Object.keys(element.props)
+    .filter(isHtmlProp)
+    .forEach(name => {
+      dom[name] = element.props[name];
+    });
+  // Attach node to the container
+  container.appendChild(dom);
+}
+
+function isHtmlProp(key) {
+  return key !== "children";
+}
+
+let nextUnitOfWork = null;
+function workLoop(deadline) {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(nextWork) {
+
 }
 
 export default {
-    createElement,
-    render
+  createElement,
+  render
 };
 
-export {
-    createElement,
-    render
-};
+export { createElement, render };
--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 /** @jsx createElement */
-import Keact, {createElement} from './src/keact';
+import Keact, { createElement } from "./src/keact";
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 const elem = (
-    <div className='test'>
-        <div>Hello</div>
-        <div>World</div>
-    </div>
-)
+  <div className="test">
+    <div>Hello</div>
+    <div>World</div>
+  </div>
+);
 Keact.render(elem, root);
